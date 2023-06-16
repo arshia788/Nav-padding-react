e80d0075e37c8b1f324007d5323f5a607a2d7618
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-16 21:24:21 +0330

    working on styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Navbar from './components/Navbar';
+import Content from './components/Content';
 
 const App = () => {
   return (
-    <div>
+    <div className='py-2'>
       <Navbar />
-      
+      <Content />
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className='flex justify-between item-center px-12 bg-blue-900 text-white'>
+        <nav className= {`flex sticky top-0 z-10 justify-between item-center py-4 px-12 bg-blue-900 text-white`} >
             <h2>yoyo</h2>
 
-            <ul className='flex items-center'>
+            <ul className='flex items-center gap-x-12'>
                 <li>home</li>
                 <li>shop</li>
                 <li>log-out</li>
