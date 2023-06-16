4d53f489eb2c817840898f1959eb66b31f51a6e5
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-16 21:27:29 +0330

    working on state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Navbar = () => {
+
+    const [show, setShow]= useState(false);
+    console.log(show);
+
+    const paddingFunc=()=>{
+        if(window.pageYOffset > 400){
+            setShow(true)
+        }else{
+            setShow(false)
+        }
+    }
+
+    useEffect(()=>{
+
+        window.addEventListener('scroll', paddingFunc)
+
+    },[show])
+
     return (
-        <nav className= {`flex sticky top-0 z-10 justify-between item-center py-4 px-12 bg-blue-900 text-white`} >
+        <nav className= {`flex  sticky top-0 z-10 justify-between item-center py-4 px-12 bg-blue-900 text-white`} >
             <h2>yoyo</h2>
 
             <ul className='flex items-center gap-x-12'>
